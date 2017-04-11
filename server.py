d7281ba4b3e70a3b2f37c6370261cc8c6942bc41
sfjordan <sfjordan@centserv.(none)>
2017-04-10 21:12:19 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,67 @@
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import socket, os
+
+class MyHandler(SimpleHTTPRequestHandler):
+  def do_GET(self):
+    try:
+      if self.path == '/ip':
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('Your IP address is %s' % self.client_address[0])
+        return
+      if self.path == '/ping':
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('<h>PONG</p>')
+        return
+      if self.path == '/network':
+        speed_img = "network_speed/speed_data.png"
+        pktloss_img = "network_speed/pktloss_data.png"
+        lat_img = "network_speed/lat_data.png"
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('<img src='+speed_img+'/>')
+        self.wfile.write('<img src='+pktloss_img+'/>')
+        self.wfile.write('<img src='+lat_img+'/>')
+        return
+      if self.path == '/speed':
+        img = os.sep+'home'+os.sep+'sfjordan'+os.sep+\
+            'network_speed'+os.sep+'speed_data.png'
+        f = open(img)
+        self.send_response(200)
+        self.send_header('Content-type', 'image/png')
+        self.send_headers()
+        self.wfile.write(f.read())
+        f.close()
+        return
+      if self.path == '/pktloss':
+        img = os.sep+'home'+os.sep+'sfjordan'+os.sep+\
+            'network_speed'+os.sep+'pktloss_data.png'
+        f = open(img)
+        self.send_response(200)
+        self.send_header('Content-type', 'image/png')
+        self.end_headers()
+        self.wfile.write(f.read())
+        f.close()
+        return
+      else:
+        return SimpleHTTPRequestHandler.do_GET(self)
+    except IOError:
+      self.send_error(404, 'File Not Found: %s' % self.path)
+
+class HTTPServerV6(HTTPServer):
+  address_family = socket.AF_INET6
+
+def main():
+  server = HTTPServerV6(('::', 80), MyHandler)
+  server.serve_forever()
+
+if __name__ == '__main__':
+  main()
+
+
+
---
+++ speed.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python3.4
+import os, sys, csv, time, logging, subprocess, traceback
+import datetime as DT
+from matplotlib.dates import date2num
+import matplotlib.pyplot as plt
+
+LOG_FILE = "/home/sfjordan/network_speed/speed_data.log"
+
+SPEED_CSV_FILE = "/home/sfjordan/network_speed/speed_data.csv"
+SPEED_IMG_FILE = SPEED_CSV_FILE[:-3]+"png"
+
+PKTLOSS_CSV_FILE = "/home/sfjordan/network_speed/pktloss_data.csv"
+PKTLOSS_IMG_FILE = PKTLOSS_CSV_FILE[:-3]+"png"
+
+LAT_CSV_FILE = "/home/sfjordan/network_speed/lat_data.csv"
+LAT_IMG_FILE = LAT_CSV_FILE[:-3]+"png"
+
+TIME_FMT_WRITE = "%Y-%m-%d %H:%M:%S"
+TIME_FMT_DISPLAY = "%x %I:%M %p"
+
+IPERF_SERV = "iperf.scottlinux.com"
+RETRY_WAIT_SECONDS = 30
+RETRY_ATTEMPTS = 20
+
+GOOGLE_DNS_V6 = "2001:4860:4860::8888"
+PING_COUNT = 100
+
+def cmd_retry(cmd_list):
+  try:
+    for attempt in range(RETRY_ATTEMPTS):
+      try:
+        res = subprocess.Popen(
+            cmd_list,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        output, error = res.communicate()
+
+        # check for errors
+        if res.poll() or not output:
+          raise subprocess.CalledProcessError(
+              res.poll(),
+              cmd_list,
+              error.decode().strip()
+          )
+        else:
+          return output, ""
+      except subprocess.CalledProcessError as e:
+        logging.error(
+            "command {} failed [ERR: {}], reporting: {}".format(
+              e.cmd, e.returncode, e.output
+            )
+        )
+        logging.error("Failed on attempt {}".format(attempt+1))
+        time.sleep(RETRY_WAIT_SECONDS)
+    else:
+      logging.critical("All attempts at command {} failed!".format(cmd_list))
+      return "", "All attempts failed"
+  except Exception as e:
+    logging.critical("Uncaught error: {}!".format(e))
+    logging.critical(traceback.format_exc())
+
+def speed_test():
+  logging.info("beginning speed tests")
+  cmd_list = ["iperf3", "-c", IPERF_SERV]
+  output, error = cmd_retry(cmd_list)
+  download = 0
+  upload = 0
+  if not error:
+    lines = str(output).split("\\n")
+    download = lines[-5].split()[4]
+    upload = lines[-4].split()[4]
+
+  logging.info("download: {} Mbps".format(download))
+  logging.info("upload: {} Mbps".format(upload))
+  return download, upload
+
+def ping_test():
+  logging.info("beginning ping tests")
+  cmd_list = ["ping6", "-c", str(PING_COUNT), GOOGLE_DNS_V6]
+  output, error = cmd_retry(cmd_list)
+  pktloss = 100
+  latency = 0
+  if not error:
+    lines = str(output).split()
+    pktloss = lines[-9].strip("%")
+    latency = lines[-2].split("/")[1]
+
+  logging.info("Packet loss: {} %".format(pktloss))
+  logging.info("Avg latency: {} ms".format(latency))
+  return pktloss, latency
+
+def write(csv_file, data):
+  # save the data to file for local network plotting
+  # data should be a list
+  logging.info("writing results to file: {}".format(csv_file))
+  out_file = open(csv_file, "a")
+  writer = csv.writer(out_file)
+  write_data = [time.strftime(TIME_FMT_WRITE)]
+  write_data.extend(data)
+  writer.writerow(write_data)
+  out_file.close()
+
+def draw(csv_file, img_file):
+  logging.info("reading results file {} and drawing".format(csv_file))
+  y, x, xticks, y_max = [], [], [], []
+  csv_reader = csv.reader(open(csv_file))
+
+  isLine2 = False
+  for line in csv_reader:
+    line_time = DT.datetime.strptime(line[0], TIME_FMT_WRITE)
+    x.append(line_time)
+    xticks.append("")
+    y.append(float(line[1]))
+    if len(line) > 2:
+      isLine2 = True
+      y_max.append(float(line[2]))
+
+  fig = plt.figure(figsize=(10,5), dpi=150)
+  ax = fig.add_subplot(111)
+  fig.subplots_adjust(bottom=0.35)
+
+  date_as_numbers = [date2num(date) for date in x]
+  line1 = ax.plot(date_as_numbers, y, label="Down")
+  if isLine2:
+    line2 = ax.plot(date_as_numbers, y_max, label="Up")
+
+  legend = ax.legend(loc="lower right", shadow=True)
+  frame = legend.get_frame()
+  frame.set_facecolor("0.90")
+
+  plt.xticks(x, xticks)
+  plt.xticks(rotation="vertical")
+
+  fig.savefig(img_file, bbox_inches="tight")
+
+def init_logging():
+  logging.basicConfig(format="%(asctime)s[%(levelname)s]: %(message)s",
+                      datefmt=TIME_FMT_WRITE,
+                      filename=LOG_FILE,
+                      level=logging.INFO,
+  )
+
+def main():
+  init_logging()
+  logging.info("script started")
+
+  down, up = speed_test()
+  write(SPEED_CSV_FILE, [down, up])
+  draw(SPEED_CSV_FILE, SPEED_IMG_FILE)
+
+  pktloss, latency = ping_test()
+  write(PKTLOSS_CSV_FILE, [pktloss])
+  draw(PKTLOSS_CSV_FILE, PKTLOSS_IMG_FILE)
+
+  write(LAT_CSV_FILE, [latency])
+  draw(LAT_CSV_FILE, LAT_IMG_FILE)
+
+  logging.info("script finished")
+
+if __name__ == "__main__":
+  main()
